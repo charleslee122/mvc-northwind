b3e3cee2ca7cf1a47779dea381d1cbefe5bc8fef
charleslee122 <tsukimi0122@gmail.com>
2017-03-05 13:07:38 +0800

    addLogonTableModelControllerVIew

---
+++ WebApplication1/SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[account]
+(
+	[accountId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [username] VARCHAR(50) NOT NULL, 
+    [password] VARCHAR(MAX) NOT NULL, 
+    [createtime] DATETIME NOT NULL, 
+    [lastlogintime] DATETIME NULL, 
+    [islogon] BIT NOT NULL, 
+    [empolyeeid] INT NOT NULL
+)
---
+++ WebApplication1/WebApplication1/Controllers/accountsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class accountsController : Controller
+    {
+        private northwindEntities1 db = new northwindEntities1();
+
+        // GET: accounts
+        public ActionResult Index()
+        {
+            return View(db.accounts.ToList());
+        }
+
+        // GET: accounts/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            account account = db.accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // GET: accounts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: accounts/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "accountId,username,password,createtime,lastlogintime,islogon,empolyeeid")] account account)
+        {
+            if (ModelState.IsValid)
+            {
+                account.accountId = Guid.NewGuid();
+                db.accounts.Add(account);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(account);
+        }
+
+        // GET: accounts/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            account account = db.accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // POST: accounts/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "accountId,username,password,createtime,lastlogintime,islogon,empolyeeid")] account account)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(account).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(account);
+        }
+
+        // GET: accounts/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            account account = db.accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // POST: accounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            account account = db.accounts.Find(id);
+            db.accounts.Remove(account);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
